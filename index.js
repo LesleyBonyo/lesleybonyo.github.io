d0b3b790c6c9485a43470b600b12eb34a6ef4cd2
LesleyBonyo <lesleybonyo@gmail.com>
2023-06-23 16:53:41 +0300

    Add javaScript

---
+++ index.js
@@ -0,0 +1,8 @@
+function changeImage(image) {
+	let myImage = document.getElementById(image);
+	myImage.style.border = "1px solid grey";
+	}
+function returnImage(image) {
+	let myImage = document.getElementById(image);
+	myImage.style.border = "none";
+	}
